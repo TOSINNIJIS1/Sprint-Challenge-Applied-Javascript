b2976498dbfaa541f851ee0d5175e4e2ce24bdb9
Vincent Adeniji <adenijitosin112@gmail.com>
2019-11-10 23:48:03 -0500

    MVP

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,29 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+
+
+const content = document.querySelector('.header-container');
+content.appendChild(Header());
+
+function Header() {
+    const hHeader = document.createElement('div');
+    const date = document.createElement('span');
+    const anotherHeader = document.createElement('h1');
+    const temperature = document.createElement('span');
+
+    hHeader.classList.add('header');
+    hHeader.classList.add('date');
+    hHeader.classList.add('temp');
+
+    hHeader.appendChild(date);
+    hHeader.appendChild(anotherHeader);
+    hHeader.appendChild(temperature);
+
+    date.textContent = 'MARCH 28, 2019';
+    anotherHeader.textContent = 'Lambda Times';
+    temperature.textContent = '98°';
+
+
+    return hHeader;
+}
